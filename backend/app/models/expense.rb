e31b6904db6bbf48ae80ea4fb8a53a08911bdf57
Gujetamaa <jerico.ibanez18@gmail.com>
2026-03-09 19:07:27 +0800

    feat: prevent future date expense creation (BONUS-001)

--- backend/app/models/expense.rb
+++ backend/app/models/expense.rb
@@ -1,3 +1,18 @@
 class Expense < ApplicationRecord
   belongs_to :category
+
+  validates :description, presence: true
+  validates :amount, presence: true
+  validates :date, presence: true
+
+  validate :date_cannot_be_in_the_future
+
+  private
+  def date_cannot_be_in_the_future
+    return if date.blank?
+
+    if date > Date.current
+      errors.add(:date, "can't be in the future")
+    end
+  end
 end
